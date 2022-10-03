78fef3364abbbfab50d2553f732e34f45a34bccc
dragmine149 <dan@thebanners.uk>
2022-10-03 10:33:13 +0100

    Added a file handler to use in saving user data
    Just there in case we need it, don't know if we will but at least we have it

---
+++ src/utils/fileHandler.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+class FileHandler:
+    def __init__(self) -> None:
+        pass
+    
+    def SaveFile(self, name, data, useJson=False):
+        """Saves a file
+
+        Args:
+            name (string): Name of the file to save
+            data (_type_): Data to save in the file
+            useJson (bool, optional): To save in a json format or not. Defaults to False.
+        """
+        with open(f"Data/{name}") as f:
+            if not useJson:
+                f.write(data)
+            else:
+                f.write(json.dumps(data))
+    
+    def ReadFile(self, name, useJson=False):
+        """Read data from a file
+
+        Args:
+            name (string): Name of the file to read from
+            useJson (bool, optional): To read the json format or not. Defaults to False.
+
+        Returns:
+            _type_: The data stored in the file
+        """
+        with open(f"Data/{name}") as f:
+            if not useJson:
+                return f.read()
+            return json.loads(f.read())
+    
+    def MakeFolder(self, name):
+        """Makes a folder
+
+        Args:
+            name (string): Name of the folder
+        """
+        try:
+            os.mkdir(f"Data/{name}")
+        except FileExistsError:
+            pass
