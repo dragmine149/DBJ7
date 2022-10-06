227e584629cf294834adc91f49080c3940b27620
dragmine149 <dan@thebanners.uk>
2022-10-06 23:25:37 +0100

    New game and module
    New game, easy to explain.
    New module for use in more future games.

---
+++ src/games/Higher_Lower.py
@@ -0,0 +1,160 @@
+import asyncio
+from src.utils.Cards import Cards
+from src.utils import uis, bank, game_template
+from discord.ext import commands
+from src.utils.MoneySelector import MoneySelector
+
+import discord
+import logging
+
+logger = logging.getLogger("game.Higher_Lower.log")
+logger.info("Initalised")
+
+# Probably don't need an unlucky factor for this game
+
+class HigherOrLower(game_template.Template):
+    """
+    Guess higher or lower on a card value
+    """
+    
+    aliases = ["HOL"]
+    name = "Higher or Lower"
+    def __init__(self, bot: commands.bot) -> None:
+        self.bot = bot
+        bank.bot = bot
+        self.currentCard = 0
+        self.correct = 0
+        
+        self.view = uis.Multiple_Buttons([
+            {
+                "label": "higher",
+                "callback": self.GuessCallback,
+                "emoji": "⬆️",
+                "style": discord.ButtonStyle.primary
+            },
+            {
+                "label": "lower",
+                "callback": self.GuessCallback,
+                "emoji": "⬇️",
+                "style": discord.ButtonStyle.secondary
+            }
+        ])
+        
+        self.embed = discord.Embed(
+            color=discord.Color.random(),
+            title="Higher Lower Progress",
+            description="Your progress report on this game",
+        )
+        
+        self.embed.add_field(
+            name="Current Card",
+            value=""
+        )
+        self.embed.add_field(
+            name="Progress",
+            value=""
+        )
+        self.embed.add_field(
+            name="Correct Guesses",
+            value=""
+        )
+    
+    @property
+    def display_emoji(self) -> str:
+        return "↕️"
+
+    async def Finish(self):
+        """
+        Finish looping through all cards, show end response
+        """
+        moneyMultiplier = 0.025 * self.correct
+        moneyReward = self.betValue * moneyMultiplier
+        
+        self.account.money += moneyReward
+        await self.Interaction.edit_original_response(content=f"You got a multiplier of {str(moneyMultiplier)[0:5]}x.\nFor a reward of {moneyReward}\n\nGame Information", embed=self.GenerateEmbed(), view=None)
+        
+    def GenerateEmbed(self) -> discord.Embed:
+        """Generate an embed with the current game progress and information
+
+        Returns:
+            discord.Embed: The embed that gets generated
+        """
+        cc = self.card.Get_Deck_Card(self.currentCard)
+        self.embed.set_field_at(
+            0, name=self.embed.fields[0].name, 
+            value=f"{cc[1]} of {cc[0]}")
+
+        self.embed.set_field_at(
+            1, name=self.embed.fields[1].name, 
+            value=f"{str(round((self.currentCard + 1) / 52, 4) * 100)[0:5]}%")
+
+        self.embed.set_field_at(
+            2, name=self.embed.fields[2].name,
+            value=self.correct
+        )
+
+        return self.embed
+    
+    async def GuessCallback(self, Interaction: discord.Interaction, label: str):
+        """Does some checks on their inputs, and other stuff
+        """
+        if Interaction.user.id != self.user.id:
+            return await Interaction.response.send_message(
+                "You can not choose an option for this user!",
+                ephemeral=True
+            )
+        
+        # Check if they won that higher or lower
+        self.currentCard += 1
+        oldCard = self.card.Get_Deck_Card(self.currentCard - 1)
+        nextCard = self.card.Get_Deck_Card(self.currentCard)
+        higher = self.card.Check_If_Higher(oldCard, nextCard)
+        highermsg = "higher" if higher else "lower"
+
+        correctmsg = "Incorrect"        
+        if label == highermsg:
+            self.correct += 1
+            correctmsg = "Correct"
+
+        # respond
+        # await Interaction.response.defer()
+        await Interaction.response.send_message(content=f"You were {correctmsg}, the new card is {highermsg} than the old card. Left: {52 - (self.currentCard + 1)}")
+        await self.HigherLower()
+        await asyncio.sleep(1)
+        await Interaction.delete_original_response()
+        
+        
+    async def HigherLower(self):
+        """Main loop
+        """
+        if self.currentCard == 51:
+            return await self.Finish()
+        
+        await self.Interaction.edit_original_response(content="", view=self.view, embed=self.GenerateEmbed())
+    
+    async def money_callback(self, value: int):
+        """Callback function after selecting amount of money
+        """
+        self.betValue = value
+        self.account = await bank.Player_Status.get_by_id(self.Interaction.user.id)
+        
+        self.account.money -= value
+        await self.HigherLower()
+    
+    async def start(self, Interaction: discord.Interaction):
+        self.user = Interaction.user
+        self.Interaction = Interaction
+        self.embed.set_author(name=self.user.display_name,
+                              icon_url=self.user.display_avatar.url)
+        
+        self.currentCard = 0
+        self.correct = 0
+
+        await Interaction.response.send_message("Loading game data...")
+        self.card = Cards()  # new deck per game
+        self.card.shuffle_cards(nUnique=True)
+        await MoneySelector(Interaction, self.money_callback).get_money()
+
+
+def game_setup(bot):
+    return HigherOrLower(bot)
---
+++ src/utils/Cards.py
@@ -0,0 +1,101 @@
+import logging
+import random
+
+logger = logging.getLogger("src.utils.Cards")
+logger.info("Initalised")
+
+
+"""
+Cards, lets you spawn in a deck of cards whenever.
+
+shuffle_cards -> Makes a deck completly full of random cards in a random order
+Draw_Card -> Draws a random card.
+Get_Deck_Card -> Returns the card at X in deck
+Check_If_Higher -> Check if B has a higher value than A
+
+These cards can be used for whatever, just do from src.utils.Cards import Cards
+"""
+
+class Cards:
+    def __init__(self):
+        self.cards = {
+            "Clubs": ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"],
+            "Spades": ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"],
+            "Diamonds": ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"],
+            "Hearts": ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"],
+        }
+        self.suits = ["Clubs", "Spades", "Diamonds", "Hearts"]
+        self.cardInfo = ["Ace", "2", "3", "4", "5", "6",
+                         "7", "8", "9", "10", "Jack", "Queen", "King"]
+        
+        self.deck = []
+    
+    def shuffle_cards(self, *, nUnique: bool = False, sUnique: bool = False):
+        """Shuffle a deck of cards
+
+        Args:
+            nUnique (bool): Determins if no two numbers can be next to each other
+            sUnique (bool): Determins if no two suits can be next to each other
+        """
+        cardInfo = self.cards.copy()
+        lastCard = None
+        
+        for i in range(52):
+            completed = False
+            while not completed:
+                card = self.Draw_Card()
+                if card not in self.deck:
+                    if lastCard is not None:
+                        # Check for unique number
+                        if nUnique:
+                            if lastCard[1] == card[1]:
+                                completed = False
+                                continue
+                        
+                        # Check for unique suit
+                        if sUnique:
+                            if lastCard[0] == card[0]:
+                                completed = False
+                                continue
+                    
+                    # Continue
+                    self.deck.append(card)
+
+                    # remove from list to make it quicker to shuffle
+                    rmCardIndex = self.cards[card[0]].index(card[1])  
+                    del self.cards[card[0]][rmCardIndex]
+
+                    if len(self.cards[card[0]]) == 0:
+                        rmIndex = self.suits.index(card[0])
+                        del self.suits[rmIndex]
+                        
+                    completed = True
+                    lastCard = None
+        
+        self.cards = cardInfo.copy()
+    
+    def Draw_Card(self):
+        """Draws a random card from the list of 52 cards
+
+        Returns:
+            _type_: _description_
+        """
+        
+        suitInd = random.randrange(len(self.suits))
+        suit = self.suits[suitInd]
+        number = random.choices(self.cards[suit])[0]
+        return (suit, number)
+    
+    def Get_Deck_Card(self, index: int):
+        if len(self.deck) > 0:
+            return self.deck[index]
+        else:
+            logger.warn("Deck has been requested for card before being shuffled")
+    
+    def Check_If_Higher(self, card, newCard):
+        newValue = newCard[1]
+        value = card[1]
+        
+        return self.cardInfo.index(newValue) >= self.cardInfo.index(value)
+        
+        
