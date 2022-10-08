e2ef05238e1e0bcd284a527caea6ac19e3224313
timelessnesses <mooping3roblox@gmail.com>
2022-10-08 10:58:11 +0700

    enum

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -16,17 +16,3 @@ class Coin_State(aenum.MultiValueEnum):
     def __str__(self):
         return self.name
 
-
-class Shop(aenum.Enum):
-    _init_ = "value __doc__ __name__"
-    lucky_potion = 0, "Lucky Potion", "Lucky Potion"
-    specific_lucky_potion = (
-        1,
-        "Lucky potion for a specific game",
-        "Specific Lucky Potion",
-    )
-    frenzy_cash = 2, "Cash bonus multiplier", "Frenzy Cash"
-    restart = 3, "Restart by remove all of your debt and unluckiness", "Restart"
-
-    def __str__(self):
-        return self.name.replace("_", " ").title()
