2015ff80a3a75ea574f4c951ddd93bdceb8139ae
timelessnesses <mooping3roblox@gmail.com>
2022-10-08 14:20:28 +0700

    fuck bank logs

--- src/shop.py
+++ src/shop.py
@@ -57,7 +57,7 @@ class Inventory_And_Shop(commands.Cog, name="Inventory and shop"):
                 )
             )
         account.money -= money
-        for x in range(amount):
+        for _ in range(amount): # type: ignore
             account.inventory.items.append(item.value)
         await ctx.reply(
             embed=discord.Embed(
@@ -95,6 +95,85 @@ class Inventory_And_Shop(commands.Cog, name="Inventory and shop"):
             )
         )
 
-
+    @shop.command()
+    async def send_money(self, ctx: commands.Context, user: discord.User, amount: int):
+        account = await bank.Player_Status.get_by_id(ctx.author.id)
+        if account.money < amount:
+            return await ctx.reply(
+                embed=discord.Embed(
+                    title="Error",
+                    description=f"You don't have enough coins to send this amount of money",
+                    color=discord.Color.red(),
+                )
+            )
+        account.money -= amount
+        account = await bank.Player_Status.get_by_id(user.id)
+        account.money += amount
+        await ctx.reply(
+            embed=discord.Embed(
+                title="Success",
+                description=f"You have sent {amount} coins to {user.mention}",
+                color=discord.Color.green(),
+            )
+        )
+    
+    @commands.hybrid_group()
+    async def inventory(self, ctx: commands.Context):
+        """Inventory commands"""
+        
+    @inventory.command()
+    async def list(self, ctx: commands.Context):
+        """List your inventory"""
+        await ctx.defer()
+        account = await bank.Player_Status.get_by_id(ctx.author.id)
+        counts = {}
+        embed = discord.Embed(title="Inventory", description="List of your items")
+        for item in account.inventory.items:
+            try:
+                counts[item] += 1
+            except KeyError:
+                counts[item] = 1
+        for item, count in counts.items():
+            embed.add_field(
+                name=Items(item).name,
+                value=f"Amount: {count}\nDescription: {Items(item).__doc__}",
+            )
+        await ctx.send(embed=embed)
+    
+    @inventory.command()
+    async def use(self, ctx: commands.Context, item: Items):
+        """Use an item"""
+        account = await bank.Player_Status.get_by_id(ctx.author.id)
+        if item.value not in account.inventory.items:
+            return await ctx.reply(
+                embed=discord.Embed(
+                    title="Error",
+                    description=f"You don't have this item in your inventory",
+                    color=discord.Color.red(),
+                )
+            )
+        account.inventory.items.remove(item.value)
+        account.effects.append(item.value)
+        item.value.activate()
+        await ctx.reply(
+            embed=discord.Embed(
+                title="Success",
+                description=f"You have used {item.name}, you can check your effects with `{ctx.prefix}inventory effects`",
+                color=discord.Color.green(),
+            )
+        )
+    @inventory.command()
+    async def effects(self, ctx: commands.Context):
+        """List your effects"""
+        await ctx.defer()
+        account = await bank.Player_Status.get_by_id(ctx.author.id)
+        embed = discord.Embed(title="Effects", description="List of your effects")
+        for effect in account.effects:
+            embed.add_field(
+                name=Items(effect).name,
+                value=f"Description: {Items(effect).__doc__}\nExpires in: {datetime.now() - effect.expire_time}",
+            )
+        await ctx.send(embed=embed)
+    
 async def setup(bot: commands.Bot):
     await bot.add_cog(Inventory_And_Shop(bot))
