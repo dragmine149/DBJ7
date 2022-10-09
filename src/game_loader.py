34bf65fe7678835112b872367c4d367508da77ef
dragmine149 <dan@thebanners.uk>
2022-10-09 09:52:13 +0100

    Fixed missed comma

--- src/game_loader.py
+++ src/game_loader.py
@@ -223,7 +223,7 @@ class game_loader(commands.Cog, name="Games"):  # type: ignore
             )
         view = uis.DropdownView(
             callback=self.game_preLoad,
-            owner=ctx.author
+            owner=ctx.author,
             placeholder="Select game to play",
             options=gameOptions,
         )
